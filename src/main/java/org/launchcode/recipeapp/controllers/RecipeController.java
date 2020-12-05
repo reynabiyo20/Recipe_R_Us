9765b73af92cc4f6b2e013374e744475574c4e4e
Oksana999 <oksanam999@gmail.com>
2020-12-05 13:31:43 -0600

    Add edit and delete features

--- src/main/java/org/launchcode/recipeapp/controllers/RecipeController.java
+++ src/main/java/org/launchcode/recipeapp/controllers/RecipeController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -85,6 +86,65 @@ public class RecipeController {
       }
 
       return "recipes/display";
+   }
+
+   @GetMapping("edit/{recipeId}")
+   public String displayEditForm(Model model, @PathVariable int recipeId) {
+
+      Category[] categories = Category.values();
+      Tag[] tags = Tag.values();
+      Optional<Recipe> recipeOpt = recipeRepository.findById(recipeId);
+      if (recipeOpt.isPresent()) {
+         Recipe recipe = recipeOpt.get();
+         model.addAttribute("recipe", recipe);
+         model.addAttribute("title", "Edit recipe " + recipe.getName());
+         model.addAttribute("recipeId", recipe.getId());
+      } else {
+         model.addAttribute("recipe", new Recipe());
+      }
+      model.addAttribute("categories", categories);
+      model.addAttribute("tags", tags);
+
+      return "recipes/edit";
+
+   }
+
+   @PostMapping("edit")
+   public String processEditForm(Integer recipeId, @ModelAttribute @Valid Recipe newRecipe,
+                                 Errors errors, Model model, RedirectAttributes redirectAttrs) {
+      if (errors.hasErrors()) {
+         model.addAttribute("title", "Edit Recipe");
+         return "recipes/edit";
+      }
+      Optional<Recipe> recipeOpt = recipeRepository.findById(recipeId);
+      if (recipeOpt.isPresent()) {
+         Recipe recipe = recipeOpt.get();
+         recipe.setCategory(newRecipe.getCategory());
+         recipe.setDirections(newRecipe.getDirections());
+         recipe.setImg(newRecipe.getImg());
+         recipe.setIngredients(newRecipe.getIngredients());
+         recipe.setName(newRecipe.getName());
+         recipe.setTag(newRecipe.getTag());
+
+
+         Recipe savedRecipe = recipeRepository.save(recipe);
+         Iterable<Recipe> recipes = recipeRepository.findAll();
+
+         redirectAttrs.addAttribute("recipes", recipes);
+
+      }
+      return "redirect:";
+
+   }
+
+   @RequestMapping("/delete/{recipeId}")
+   public String handleDeleteUser(@PathVariable Integer recipeId) {
+      Optional<Recipe> recipeOpt = recipeRepository.findById(recipeId);
+      if (recipeOpt.isPresent()) {
+         recipeRepository.deleteById(recipeId);
+      }
+
+      return "redirect:/recipes";
    }
 
 }
