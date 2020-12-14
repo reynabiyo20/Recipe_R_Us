b1bdb9a6c8241c5fdcad6fd6c7c7c5138c6f310a
Oksana999 <oksanam999@gmail.com>
2020-12-14 03:31:02 -0600

    Add possibility to save favorite recipes

--- src/main/java/org/launchcode/recipeapp/controllers/AuthenticationController.java
+++ src/main/java/org/launchcode/recipeapp/controllers/AuthenticationController.java
@@ -27,22 +27,11 @@ AuthenticationController {
     private  static final String userSessionKey = "user";
 
     public User getUserFromSession(HttpSession session) {
-        Integer userId = (Integer) session.getAttribute(userSessionKey);
-        if (userId == null) {
-            return null;
-        }
-
-        Optional<User> user = userRepository.findById(userId);
-
-        if (user.isEmpty()) {
-            return null;
-        }
-
-        return user.get();
+        return (User) session.getAttribute(userSessionKey);
     }
 
     private static void setUserInSession(HttpSession session, User user) {
-        session.setAttribute(userSessionKey, user.getId());
+        session.setAttribute(userSessionKey, user);
     }
 
     @GetMapping("/register")
--- src/main/java/org/launchcode/recipeapp/models/data/UserRepository.java
+++ src/main/java/org/launchcode/recipeapp/models/data/UserRepository.java
@@ -1,13 +1,12 @@
 package org.launchcode.recipeapp.models.data;
 
-
 import org.launchcode.recipeapp.models.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.transaction.Transactional;
-
 @Repository
-public interface UserRepository extends CrudRepository<User, Integer>{
-    User findByUsername(String username);
+public interface UserRepository extends CrudRepository<User, Integer> {
+   User findByUsername(String username);
+
+   User getById(Integer id);
 }
