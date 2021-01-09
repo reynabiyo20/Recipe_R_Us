e140d550c8a16913076a57c5e441cb125c0f425a
Paal <paalwilliams@gmail.com>
2021-01-08 19:59:42 -0500

    update famous saint louis recipe, form validation and creation

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -7,26 +7,42 @@ if(addIngredientButton) {
     addIngredientButton.addEventListener('click', (e) => {
         e.preventDefault();
 
-        let quantityEl = document.getElementById('quantity')
-        let quanClone = quantityEl.cloneNode(true);
-        ingredientsContainer.appendChild(quanClone);
+        let newQuantity = document.createElement("input");
+        newQuantity.setAttribute("name", "quantity");
+        newQuantity.setAttribute('id', 'quantity')
+        newQuantity.setAttribute('type', 'number');
+        newQuantity.setAttribute('min', '1');
+        newQuantity.setAttribute('step', '.1')
+        newQuantity.setAttribute("placeholder", "Quantity")
+        newQuantity.required = true;
+        ingredientsContainer.appendChild(newQuantity);
+
+
 
         let measurementEl = document.getElementById('measurement');
         let msrClone = measurementEl.cloneNode(true);
         ingredientsContainer.appendChild(msrClone);
 
-        let ingredientEl = document.getElementById('ingredient')
-        let ingredClone = ingredientEl.cloneNode(true);
-        ingredientsContainer.appendChild(ingredClone);
+        let newIngredient = document.createElement("input");
+        newIngredient.setAttribute("name", "ingredient");
+        newIngredient.setAttribute('id', 'ingredient')
+        newIngredient.setAttribute('type', 'text');
+        newIngredient.setAttribute("placeholder", "Ingredient")
+        newIngredient.required = true;
+        ingredientsContainer.appendChild(newIngredient);
+
     })
 }
 
 if(addInstructionButton) {
     addInstructionButton.addEventListener('click', (e) => {
         e.preventDefault();
-        let instructionEl = document.getElementById('instruction');
-        let instructClone = instructionEl.cloneNode(true);
-        instructClone.setAttribute('value', '')
-        instructionsContainer.appendChild(instructClone);
+        let newInstruction = document.createElement('input');
+        newInstruction.setAttribute("name", "instruction");
+        newInstruction.setAttribute('id', 'instruction')
+        newInstruction.setAttribute('type', 'text');
+        newInstruction.setAttribute("placeholder", "Instruction")
+        newInstruction.required = true;
+        instructionsContainer.appendChild(newInstruction);
     })
 }
