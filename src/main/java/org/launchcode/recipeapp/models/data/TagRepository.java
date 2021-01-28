9e0fb78b9ea0b6005fd962aefa1b56a687170d4a
mthreec1983 <51465616+mthreec1983@users.noreply.github.com>
2021-01-28 16:55:09 -0600

    Revert "Revert "Add shopping-list functionality + fix tags duplication""

--- src/main/java/org/launchcode/recipeapp/models/data/TagRepository.java
+++ src/main/java/org/launchcode/recipeapp/models/data/TagRepository.java
@@ -4,7 +4,10 @@ import org.launchcode.recipeapp.models.Tag;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TagRepository extends CrudRepository<Tag, Integer> {
+    List<Tag> findAll();
 
     }
