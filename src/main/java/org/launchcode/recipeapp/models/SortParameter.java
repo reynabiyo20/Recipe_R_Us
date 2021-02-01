fe229243849329a2f3dda60a101da282de136250
reyna biyo <reynabiyo20@gamil.com>
2021-01-31 19:30:34 -0600

    still need to fix errors 3,4(found by Kelly)

--- src/main/java/org/launchcode/recipeapp/models/SortParameter.java
+++ src/main/java/org/launchcode/recipeapp/models/SortParameter.java
@@ -4,9 +4,9 @@ public enum SortParameter {
     NAME_ASCENDING ("Recipe Name: A-Z"),
     NAME_DESCENDING ("Recipe Name: Z-A"),
     RATING_ASCENDING ("Average Rating: High-Low"),
-    RATING_DESCENDING ("Average Rating: Low-High"),
-    TIME_ASCENDING ("Total Time: High-Low"),
-    TIME_DESCENDING ("Total Time: Low-High");
+    RATING_DESCENDING ("Average Rating: Low-High");
+//    TIME_ASCENDING ("Total Time: High-Low"),
+//    TIME_DESCENDING ("Total Time: Low-High");
 
 
     private String name;
